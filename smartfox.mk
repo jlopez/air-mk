b2a7a680e383448c47c9108381a939c02d518c67
Jesus Lopez <jesus@jesusla.com>
2013-02-20 09:59:45 -0800

    smartfox update

--- smartfox.mk
+++ smartfox.mk
@@ -9,8 +9,9 @@ SFS_EXT_DIR = $(SFS_SERVER_ROOT)/extensions
 SFS_EXT_LIB_DIR = $(SFS_EXT_DIR)/__lib__
 SFS_LOG = $(SFS_LOG_DIR)/smartfox.log
 SFS_LIBS = $(wildcard $(SFS_SERVER_ROOT)/lib/*.jar)
-SFS_LIBS_PATTERN = $(addprefix %/,$(notdir $(SFS_LIBS)))
-sfsjarfilter = $(filter-out $(SFS_LIBS_PATTERN),$1)
+SFS_EXT_LIBS = $(wildcard $(SFS_EXT_LIB_DIR)/*.jar)
+sfsjarfilter = $(call excludefiles,$(SFS_LIBS) $(SFS_EXT_LIBS),$1)
+excludefiles = $(filter-out $(addprefix %/,$(notdir $1)),$2)
 
 sfs-%: | $(SFS_SERVICE)
 	$(SFS_SERVICE) $* |cat
@@ -40,7 +41,7 @@ $(call chkvars,$1_ZONE)
 $1_EXT_DIR = $$(SFS_EXT_DIR)/$$($1)
 
 $1_LIBS = $$(call sfsjarfilter,$$($2_JAR_CLASSPATH))
-$1_DST_LIBS = $$(addprefix $$(SFS_EXT_LIB_DIR)/,$$(notdir $$($1_LIBS)))
+$1_DST_LIBS = $$(addprefix $$($1_EXT_DIR)/,$$(notdir $$($1_LIBS)))
 $1_DST_ZONE = $$(SFS_ZONES_DIR)/$$($1).zone.xml
 $1_DST_JAR_ = $$($1_EXT_DIR)/extension.jar
 
@@ -49,7 +50,7 @@ sfs-install:: $$($1_DST_JAR_) $$($1_DST_ZONE) $$($1_DST_LIBS)
 
 sfs-uninstall::
 	rm -fr $$($1_EXT_DIR)
-	rm -f $$($1_DST_ZONE) $$($1_DST_LIBS)
+	rm -f $$($1_DST_ZONE)
 
 $$($1_DST_JAR_): $$($2_JAR) |$$($1_EXT_DIR)
 	$$(call silent,COPY $4 jar, \
@@ -58,11 +59,17 @@ $$($1_DST_JAR_): $$($2_JAR) |$$($1_EXT_DIR)
 $$($1_DST_ZONE): $$($1_ZONE) |$$(SFS_ZONES_DIR)
 	$$(call silent,COPY $4 zone.xml,cp $$< $$@)
 
-$$($1_DST_LIBS):: $$($1_LIBS) |$$(SFS_EXT_LIB_DIR)
-	$$(call silent,COPY $4 libs,cp $$^ $$(SFS_EXT_LIB_DIR))
+$$($1_DST_LIBS):: $$($1_LIBS) |$$($1_EXT_DIR)
+	$$(call silent,COPY $4 libs,cp $$^ $$($1_EXT_DIR))
 
 $$($1_EXT_DIR):
 	$$(call silent,MKDIR $$@,mkdir $$@)
+
+sfs-mkdebug::
+	@echo "$1_LIBS: $$($1_LIBS)"
+	@echo "$1_DST_LIBS: $$($1_DST_LIBS)"
+	@echo "$1_DST_ZONE: $$($1_DST_ZONE)"
+	@echo "$1_DST_JAR_: $$($1_DST_JAR_)"
 
 endef
 
